5a6e64bd25e45ead65c8174d13a576ab7e38768a
Paul Hendry <paul@pshendry.com>
2017-07-05 22:10:16 -0700

    psh-xps13: Use latest available Linux kernel

--- systems/psh-xps13/configuration.nix
+++ systems/psh-xps13/configuration.nix
@@ -50,6 +50,7 @@
       efi.canTouchEfiVariables = true;
       timeout = 0;
     };
+    kernelPackages = pkgs.linuxPackages_latest;
   };
   fileSystems."/".options = [ "noatime" "nodiratime" ];
 
